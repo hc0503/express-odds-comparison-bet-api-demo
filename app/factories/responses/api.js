4402bd2c0753dd353d543fb7f8baa0a17c1999e4
Devdreamsolution <q3construction1@gmail.com>
2021-07-07 22:19:42 +0900

    Update api response function

--- app/factories/responses/api.js
+++ app/factories/responses/api.js
@@ -1,82 +1,27 @@
-const FORMATS = {
-	JSON: "JSON",
-	XML: "XML"
+function _generateResponse(options = {res: null, status: 200, data: {}, errors: {}, msg: "", success: true}) {
+	return options.res.status(options.status).json({
+		"success": options?.success ?? true,
+		"data": options?.data ?? {},
+		"errors": options?.errors ?? {},
+		"msg": options?.msg ?? ""
+	});
 }
-
+function _createOKResponse(options) {
+	return _generateResponse({
+		...options,
+		status: 200,
+		success: true
+	})
+}
+function _createErrorResponse(options) {
+	return _generateResponse({
+		...options,
+		status: options.status ?? 500,
+		success: false
+	})
+}
 
 module.exports = {
 	createOKResponse: _createOKResponse,
 	createErrorResponse: _createErrorResponse
 }
-
-/*
-	Format for all API responses will be JSON
-	{
-		data: {...}
-		error: {...}
-	}
-	Status code is sent in header.
-
-	If error is not present, error should be null.
-	If error is present, data can be null (But it's not required).
-*/
-function _createGenericResponse(options = { res: null, status: 200, data: {}, error: null, format: FORMATS.JSON }) {
-	try {
-		const data = {
-			data: options?.data ?? null,
-			error: options?.error ?? null
-		};
-
-		switch (options?.format) {
-			case FORMATS.JSON:
-				return options?.res.status(options?.status).json(data);
-			case FORMATS.XML:
-				break;
-			default: {
-				const err = new Error("No response format specified.");
-				throw err;
-			}
-		}
-	}
-	catch (error) {
-		const err = new Error(`Could not create generic response: ${error.message}`);
-		err.name = error?.name;
-		err.code = error?.code;
-		throw err;
-	}
-}
-
-/**
- * Sends response with status code 200.
- * Should be called on all successful respones.
- *
- * @param <Object> res
- * @param <Object> data
- * @param <String> format
- */
-function _createOKResponse(options) {
-	return _createGenericResponse({
-		...options,
-		status: 200,
-		format: options?.format ?? FORMATS.JSON
-	});
-}
-
-/**
- * Sends response with provided error code.
- * Should be called on all failed respones.
- *
- * @param <Object> res
- * @param <Object> error
- * @param <Object> data (optional)
- * @param <Int>		 status
- * @param <String> format
- */
-function _createErrorResponse(options) {
-	return _createGenericResponse({
-		...options,
-		status: options?.status ?? 500,
-		format: options?.format ?? FORMATS.JSON
-	});
-}
-
